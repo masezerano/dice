2c91b18ab464e07237245efd95b711ea1e9ae753
masezerano <masezeranotaylor@gmail.com>
2023-07-21 12:29:39 +0200

    new homepage.dart

---
+++ lib/homepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: ListView(
+        children: [
+          TaskList(), 
+        ]
+      ),
+    );
+  }
+}
